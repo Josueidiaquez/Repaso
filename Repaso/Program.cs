35e9a52b9a24b1ebc1be1efb9f74a39f14830e3f
Josueidiaquez <josueidiaquez4@gmail.com>
2024-09-05 13:54:48 -0600

    Agregar archivos de proyecto.

---
+++ Repaso/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Repaso
+{
+    static void Main(string[] args)
+    {
+        string Nombre;
+        Console.WriteLine("Bienvenido al sistema de ventas");
+        Console.WriteLine("Ingrese su nombre;");
+        Nombre = Console.ReadLine();
+        Console.WriteLine("Ingrese su edad");
+    }
+}
